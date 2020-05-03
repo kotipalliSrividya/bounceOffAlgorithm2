68b25d791fc6d5b56b80ef31e651561c5d24c900
kotipalliSrividya <63097838+kotipalliSrividya@users.noreply.github.com>
2020-05-03 10:28:43 +0530

    hi shabaz, i made some spaces in your code.

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function draw() {
   bounceOff(movingRect,fixedRect);
   drawSprites();
 }
-
+a
 function bounceOff(object1,object2){
   if (object1.x - object2.x < object2.width/2 + object1.width/2
     && object2.x - object1.x < object2.width/2 + object1.width/2) {
